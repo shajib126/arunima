04dec65c06b43bc23bae1be4044a79ae25fc723d
shajib126 <shajib126@gmail.com>
2023-03-27 23:14:18 +0600

    updated config file

--- app.js
+++ app.js
@@ -10,7 +10,9 @@ const errorMiddleware = require("./middleware/error");
 
 // Config
 if (process.env.NODE_ENV !== "PRODUCTION") {
-  require("dotenv").config({ path: "backend/config/config.env" });
+  require("dotenv").config({ path: "config/config.env" });
+}else{
+  require("dotenv").config({ path: "config/config.env" });
 }
 
 app.use(express.json());
